83caf9e29ac0fb0fddefcd67f291d2989176f89e
jahid <jahidmorol2@gmail.com>
2023-07-21 12:50:10 +0600

    try to understand swich condition

---
+++ trySwich.js
@@ -0,0 +1,26 @@
+const rollNum = 2;
+switch (rollNum) {
+  case 1:
+    console.log("fardaws");
+    break;
+  case 2:
+    console.log("rohol");
+    break;
+  case 3:
+    console.log("rakib");
+    break;
+  case 4:
+    console.log("numan");
+    break;
+  case 5:
+    console.log("nayem");
+    break;
+  case 6:
+    console.log("jahid");
+    break;
+  case 7:
+    console.log("nadim");
+    break;
+  default:
+    console.log("ohthers");
+}
